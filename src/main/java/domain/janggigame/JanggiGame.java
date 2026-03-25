73b155a5f032eb4c561d58d8ffcf5ad4ea80e536
ohjonghyuk <ohjonghyuk0717@naver.com>
2026-03-25 17:49:38 +0900

    feat(JanggiGame): 뷰를 제외한 진형별 장기판 초기화 기능 추가
    장기판 초기화를 진행하며 각 진형별 기물 배치 초기화를 구현하였습니다.

--- src/main/java/domain/janggigame/JanggiGame.java
+++ src/main/java/domain/janggigame/JanggiGame.java
@@ -1,74 +1,30 @@
 package domain.janggigame;
 
-import domain.board.Board;
 import domain.piece.Side;
 import domain.players.Players;
-import domain.position.Move;
-import domain.position.Position;
-import view.InputView;
-import view.OutputView;
-
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
-import static util.Retry.retry;
 
 public class JanggiGame {
     private final Players players;
-    private final Board board;
 
-    public JanggiGame(Players players, Board board) {
+    public JanggiGame(Players players) {
         this.players = players;
-        this.board = board;
     }
 
     public void run() {
-        selectSide();
-        placeBoardBySide();
-        playGame();
+        HanPlayerPlacement();
+        ChoPlayerPlacement();
     }
 
-    private void playGame() {
-        retry(() -> {
-            while (!board.isFinished()) {
-                Move move = inputAndParseToMove();
-                players.playTurn(board, move);
-                OutputView.printBoard(board.findState());
-            }
-        });
+    private void HanPlayerPlacement() {
+        // 상배치 코드입력 받고
+        players.initPlacementBySide(Side.HAN, 1);
+        // 출력
     }
 
-    private Move inputAndParseToMove() {
-        Position startPosition = InputView.inputStartPosition();
-        Position endPosition = InputView.inputEndPosition();
-        return new Move(startPosition, endPosition);
+    private void ChoPlayerPlacement() {
+        // 상배치 코드입력 받고
+        players.initPlacementBySide(Side.CHO, 2);
+        // 출력
     }
 
-    private void selectSide() {
-        retry(() -> {
-            int sideCode = InputView.inputSideChoice();
-            Side side = generateSide(sideCode);
-            OutputView.printSideChoiceResult(side);
-        });
-    }
-
-    private Side generateSide(int sideCode) {
-        List<Side> sides = Arrays.asList(Side.values());
-        Collections.shuffle(sides);
-        return sides.get(sideCode - 1);
-    }
-
-    private void placeBoardBySide() {
-        initPlacementBySide(Side.HAN);
-        initPlacementBySide(Side.CHO);
-    }
-
-    private void initPlacementBySide(Side side) {
-        retry(() -> {
-            int placementCode = InputView.inputPlacementCodeBy(side);
-            players.initPlacementBySide(side, placementCode, board);
-            OutputView.printBoard(board.findState());
-        });
-    }
 }
