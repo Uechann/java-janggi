58d9453b6cf59368c043f6afa7419a8d40437dec
나의찬 <cab07036@naver.com>
2026-03-25 16:38:30 +0900

    feat(Board): 장기 보드에 초기화 기능 구현
    장기 보드에서 진영과 배치를 통해서 해당 진영의 기물들을 장기판에 알맞게 배치하도록 구현하였습니다.

--- src/main/java/domain/player/Player.java
+++ src/main/java/domain/player/Player.java
@@ -1,26 +1,19 @@
 package domain.player;
 
-import domain.board.Board;
-import domain.board.Placement;
 import domain.piece.Side;
-import domain.position.Move;
 
 public class Player {
     private final Side side;
 
-    public Player(Side side) {
+    private Player(Side side) {
         this.side = side;
+    }
+
+    public static Player of(Side side) {
+        return new Player(side);
     }
 
     public Side getSide() {
         return side;
-    }
-
-    public void initBoard(Board board, int placementCode) {
-        board.placePieces(side, Placement.from(placementCode));
-    }
-
-    public void play(Board board, Move move) {
-        board.move(move.startPosition(), move.endPosition(), side);
     }
 }
