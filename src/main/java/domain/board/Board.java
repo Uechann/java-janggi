83ab2709249402a6ced7ab9ecc177dc7f15d7605
나의찬 <cab07036@naver.com>
2026-03-26 21:23:26 +0900

    feat(Piece): 졸 기물의 이동 기능 구현
    1. 각 기물별 MovingCondition 인터페이스 구현 스켈레톤 생성
    2. PawnMovingCondition 구현체에 canMove 오버라이딩 메서드 구현
    3. Direction Enum 도입 및 기물이 시작 위치에서 도착위치로 이동할 수 있는 방향을 가진 Direction Queue 반환 메서드 구현
    4. Position 객체 record에서 final class로 수정 및 row와 column을 Coordinate record 좌표로 수정
    5. Board에서 move 메서드를 통해 시작 위치에서 도착위치로 진영별 기물 이동 판단 및 이동
    6. PieceType에 기물 종류별 MovingCondition 이동 조건 구현체 주입

--- src/main/java/domain/board/Board.java
+++ src/main/java/domain/board/Board.java
@@ -75,4 +75,25 @@ public class Board {
         state.put(Position.of(row, 7), Piece.of(side, placement.getColumnSevenType()));
         state.put(Position.of(row, 8), Piece.of(side, placement.getColumnEightType()));
     }
+
+    public void move(Position from, Position to, Side side) {
+        if (!state.containsKey(from)) {
+            throw new IllegalArgumentException("해당 위치에 기물이 없습니다.");
+        }
+
+        Piece fromPiece = state.get(from);
+        Piece toPiece = state.get(to);
+
+        if (!fromPiece.isSameSide(side)) {
+            throw new IllegalArgumentException("본인 진영의 말만 이동할 수 있습니다.");
+        }
+        if (toPiece != null && toPiece.isSameSide(side)) {
+            throw new IllegalArgumentException("본인 진영의 말은 포획할 수 없습니다.");
+        }
+
+        if (fromPiece.canMove(state, from, to)) {
+            state.put(to, fromPiece);
+            state.remove(from);
+        }
+    }
 }
